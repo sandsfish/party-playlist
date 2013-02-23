7feb651a77df79a5fbac2b8bae677bb44c1048b6
Sands Fish <sandsfish@gmail.com>
2013-02-23 17:31:27 -0500

    Initial commit of Rails framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyPlaylist::Application.config.secret_token = '62541a4e5a143be32c4fd793dc29b62b88a34a2886c106df2454eace50e490aaac5fd25ef9e0c9965d46c6b50fcc5ea8502622a13a58534f67caa439939a7c2d'
